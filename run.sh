928c87848282ea5479d37ea71f003b98f9e9b572
Tupsu-jy <ylinenjaakko@gmail.com>
2025-03-03 21:52:07 +0200

    better run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
-source venv/bin/activate
+
+# Enable error messages
+set -e
+
+# Check if the virtual environment already exists
+if [ ! -d "venv" ]; then
+    echo "Virtual environment not found. Creating a new one..."
+    python3 -m venv venv
+    source venv/bin/activate
+    echo "Installing dependencies..."
+    pip install -r requirements.txt
+else
+    echo "Virtual environment found, activating it..."
+    source venv/bin/activate
+fi
+
+# Run the Python script
+echo "Starting the script"
 python taskMonitor.py
+
+# Exit the virtual environment
 deactivate
